bcab16698f8fe9c34c6b91643d82aa3c41eb0ba0
kevinstiebich <kevin.stiebich@gmail.com>
2026-09-24 14:57:51 +0200

    Entwicklerdokumentation hinzufügen

--- lib/teilnehmer.dart
+++ lib/teilnehmer.dart
@@ -1,9 +1,22 @@
+/// Represents a participant in a group.
 class Teilnehmer {
+  /// First name.
   final String vorname;
+
+  /// Last name.
   final String nachname;
+
+  /// Final grade.
   final int? abschlussnote;
+
+  /// States whether the participant passed, or not.
   final bool hatBestanden;
 
+  /// Represents a participant in a group.
+  /// [vorname]: First name.
+  /// [nachname]: Last name.
+  /// [abschlussnote]: Final grade.
+  /// [hatBestanden]: States whether the participant passed, or not.
   Teilnehmer({
     required this.vorname,
     required this.nachname,
@@ -11,6 +24,7 @@ class Teilnehmer {
     this.hatBestanden = false,
   });
 
+  /// Copies a Teilnehmer-object with new or the same values.
   Teilnehmer copyWith({
     String? vorname,
     String? nachname,
